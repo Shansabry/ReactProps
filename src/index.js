2b69a329be28b27e7fdbb7ca882de01e3c832bcd
Shanmuga Sabareesh <shansabry@gmail.com>
2020-08-16 15:21:42 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const fakeInternetData = faker.internet;
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <h4>Warning</h4>
+        <div>Are you sure you want to do this?</div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          comment={faker.random.words()}
+          timeAgo="Today at 5:00PM"
+          avatar={fakeInternetData.avatar()}
+          author={fakeInternetData.userName()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          comment={faker.random.words()}
+          timeAgo="Today at 2:12PM"
+          avatar={fakeInternetData.avatar()}
+          author={fakeInternetData.userName()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          comment={faker.random.words()}
+          timeAgo="Yesterday at 11:12AM"
+          avatar={fakeInternetData.avatar()}
+          author={fakeInternetData.userName()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
